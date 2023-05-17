5be53a97645ec0e6f20fcddfe580b23ebe0f35d4
Garrett Lam <94739295+garrett-lam@users.noreply.github.com>
2023-05-17 14:26:04 -0700

    micromamba yaml, main

--- scRNAseq_filter.py
+++ scRNAseq_filter.py
@@ -3,10 +3,14 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # MAIN DRIVER
-if __name__ == "__main__":
+def main():
     anndata = sc.read_h5ad(filename="anndata_with_QCMetrics.h5ad")
     sns.histplot(anndata.obs["n_genes_by_counts"])
     #sns.histplot(anndata.obs["total_counts"])
     #sns.histplot(anndata.obs["pct_counts_mt"])
     plt.show()
     #print(test)
+
+if __name__ == "__main__":
+    main()
+    
