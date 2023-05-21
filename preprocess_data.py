4da00cbe54f6b5b6dc7bcd2f50e9548474534830
Garrett Lam <94739295+garrett-lam@users.noreply.github.com>
2023-05-20 17:05:35 -0700

    pbmc

--- preprocess_data.py
+++ preprocess_data.py
@@ -13,24 +13,31 @@ def preprocess_data(data_dir):
         if re.search(pattern, basename):
             datasets.append(re.search(pattern,basename).group(1)) # adds name of dataset to datasets
 
+    print(datasets)
     adatas = {}
-    for ds in datasets:
-        adatas[ds] = sc.read_10x_mtx(data_dir, prefix=ds+"_", cache=True)
-    combined = ad.concat(adatas, label="dataset")
-    combined.obs_names_make_unique()
+    adata_obj = None
+    if len(datasets) > 1:
+        for ds in datasets:
+            adatas[ds] = sc.read_10x_mtx(data_dir, prefix=ds+"_", cache=True)
+        adata_obj = ad.concat(adatas, label="dataset")
+    else:
+        for ds in datasets:
+            adata_obj = sc.read_10x_mtx(data_dir, prefix=ds+"_", cache=True)
+    
+    adata_obj.obs_names_make_unique()
 
-    print(combined.obs['dataset'])
+    print(adata_obj.obs['dataset'])
 
     # to filter cells
-    sc.pp.filter_cells(combined, min_genes=200)
-    sc.pp.filter_cells(combined, min_counts=1000)
+    sc.pp.filter_cells(adata_obj, min_genes=200)
+    sc.pp.filter_cells(adata_obj, min_counts=1000)
 
     # to filter genes
-    sc.pp.filter_genes(combined, min_cells=5)
-    sc.pp.filter_genes(combined, min_counts=15)
+    sc.pp.filter_genes(adata_obj, min_cells=5)
+    sc.pp.filter_genes(adata_obj, min_counts=15)
 
-    combined.var['mt'] = combined.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
-    sc.pp.calculate_qc_metrics(combined, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
+    adata_obj.var['mt'] = adata_obj.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
+    sc.pp.calculate_qc_metrics(adata_obj, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
 
-    print(f'Number of Genes in Combined (after filtering): {combined.n_vars}')
-    print(f'Number of Cells in Combined (after filtering): {combined.n_obs}')
+    print(f'Number of Genes in adata_obj (after filtering): {adata_obj.n_vars}')
+    print(f'Number of Cells in adata_obj (after filtering): {adata_obj.n_obs}')
