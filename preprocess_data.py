00bac20040ffd1c3c7fa1c63bd2c01e066e24e2c
gordonyye <gordon.y.ye@gmail.com>
2023-06-02 20:56:33 -0700

    formatting

--- preprocess_data.py
+++ preprocess_data.py
@@ -8,15 +8,20 @@ import os
 def preprocess_data(data_dir):
     # container for each particular dataset
     datasets = [] 
-    # reg ex 
+    
+    # regex 
     pattern = r"(.*)_features" 
 
-    # added all the datasets to the container
+    # add all the datasets to the container
     for file in glob.glob(data_dir + '*'):
-        basename = os.path.basename(file) # gets name of file without whole path
-        # search file and check if file is the feature file
+        ## get name of file without whole path
+        basename = os.path.basename(file)
+
+        ## search file and check if file is the feature file
         if re.search(pattern, basename):
-            datasets.append(re.search(pattern,basename).group(1)) # adds name of dataset to datasets
+
+            ## add name of dataset to datasets
+            datasets.append(re.search(pattern,basename).group(1))
 
     # container to hold datasets 
     adatas = {}
@@ -25,20 +30,22 @@ def preprocess_data(data_dir):
     # for each dataset read in data
     for ds in datasets:
         adatas[ds] = sc.read_10x_mtx(data_dir, prefix=ds+"_", cache=True)
+    
     # combine dataset into one adata object
     adata_obj = ad.concat(adatas, label="dataset")
     adata_obj.obs_names_make_unique()
 
-    # to filter cells
+    # filter cells
     sc.pp.filter_cells(adata_obj, min_genes=200)
     sc.pp.filter_cells(adata_obj, min_counts=1000)
 
-    # to filter genes
+    # filter genes
     sc.pp.filter_genes(adata_obj, min_cells=5)
     sc.pp.filter_genes(adata_obj, min_counts=15)
 
     # replace mitrochrondial genes
-    adata_obj.var['mt'] = adata_obj.var_names.str.startswith('MT-')  # annotate the group of mitochondrial genes as 'mt'
+    adata_obj.var['mt'] = adata_obj.var_names.str.startswith('MT-') 
+
     # calculate metrics
     sc.pp.calculate_qc_metrics(adata_obj, qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
 
